db14aa73c762655c8e32f93d19aad1a9b3e6d6cc
TiagoTR <tiagorocha.tr16@gmail.com>
2020-10-28 20:25:07 -0300

    Pagina erro 404 finalizada

--- src/pages/Error404/index.js
+++ src/pages/Error404/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import HeaderDashboard from '../../components/HeaderDashboard';
 
 import avisoImg from '../../imgs/icons/aviso.png';
 import './styles.css';
 
 function Error404 () {
     return (
-        <div id="pagina-geral">
-            <div>
-                <img src={avisoImg}/>
-                <h1>ERRO 404</h1>
-                <h1>PÁGINA NÃO ENCONTRADA!</h1>
-            </div>    
+        <div>
+            <HeaderDashboard/>
+            <div id="body-error-page">
+                <div>
+                    <img src={avisoImg}/>
+                    <h1>ERRO 404</h1>
+                    <h1>PÁGINA NÃO ENCONTRADA!</h1>
+                </div>    
+            </div>
         </div>
     );
 }
